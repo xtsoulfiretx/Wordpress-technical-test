f80373cd87d42c916985173ad227e29f322e861f
xtsoulfiretx <alexi.lambrou@gmail.com>
2024-08-06 16:22:32 +0100

    Add new meta boxes
    add meta box custom field functionality.

--- wp-content/plugins/pies-plugin/Includes/pies-functions.php
+++ wp-content/plugins/pies-plugin/Includes/pies-functions.php
@@ -20,7 +20,7 @@ array( 'labels' => array (
 add_action( 'init', 'create_posttype' );
 /* Custom Post Type End */
 
-/*Custom Pies type start*/
+/*Custom Pies post type start*/
 function post_type_pies() {
     $supports = array(
     'title', // Post title
@@ -51,13 +51,65 @@ function post_type_pies() {
     'has_archive' => true,
     'hierarchical' => false,
     'show_in_rest' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'show_in_admin_bar' => true,
+    'has_archive' => true,
+    'exclude_from_search' => false,
+    'publicly_queryable' => true,
     );
     register_post_type('pies', $args);
     }
     add_action('init', 'post_type_pies');
-    /*Custom Pies type end*/
+    /*Custom Pies post type end*/
 
+    // Add meta boxes
+    function add_pies_meta_boxes() {
+        add_meta_box('pies_meta_box', 'Pie Details', 'display_pies_meta_box', 'pies', 'normal', 'high');
+    }
+    add_action('add_meta_boxes', 'add_pies_meta_boxes');
 
+    // Display the meta box
+    function display_pies_meta_box($post) {
+        $pie_type = get_post_meta($post->ID, 'pie_type', true);
+        $description = get_post_meta($post->ID, 'description', true);
+        $ingredients = get_post_meta($post->ID, 'ingredients', true);
+        ?>
+        <label for="pie_type">Pie Type:</label>
+        <input type="text" name="pie_type" value="<?php echo esc_attr($pie_type); ?>" class="widefat" /><br/><br/>
+
+        <label for="description">Description:</label>
+        <textarea name="description" class="widefat"><?php echo esc_textarea($description); ?></textarea><br/><br/>
+
+        <label for="ingredients">Ingredients:</label>
+        <textarea name="ingredients" class="widefat"><?php echo esc_textarea($ingredients); ?></textarea>
+        <?php
+    }
+
+    // Save the meta box data
+    function save_pies_meta_box($post_id) {
+        // Check the user's permissions.
+        if ('pies' == $_POST['post_type']) {
+            if (!current_user_can('edit_page', $post_id)) {
+                return $post_id;
+            }
+        } else {
+            if (!current_user_can('edit_post', $post_id)) {
+                return $post_id;
+            }
+        }
+
+        // Sanitize user input.
+        $pie_type = sanitize_text_field($_POST['pie_type']);
+        $description = sanitize_textarea_field($_POST['description']);
+        $ingredients = sanitize_textarea_field($_POST['ingredients']);
+
+        // Update the meta field in the database.
+        update_post_meta($post_id, 'pie_type', $pie_type);
+        update_post_meta($post_id, 'description', $description);
+        update_post_meta($post_id, 'ingredients', $ingredients);
+    }
+    add_action('save_post', 'save_pies_meta_box');
 
     /*Change title placeholder text */
     function pies_change_title_text( $title ){
@@ -88,23 +140,15 @@ function post_type_pies() {
         // Extract the 'lookup' attribute.
         $lookup = sanitize_text_field($atts['lookup']);
 
-        // Sample data for pies.
-        $pies = array(
-            'apple' => 'Apple Pie',
-            'cherry' => 'Cherry Pie',
-            'pumpkin' => 'Pumpkin Pie',
-            'blueberry' => 'Blueberry Pie',
-        );
-
         $output = '';
 
         // If 'lookup' is provided and exists in sample data, return the corresponding pie.
-        if ($lookup && array_key_exists($lookup, $pies)) {
-            $output = '<div>' . esc_html($pies[$lookup]) . '</div>';
+        if ($lookup && array_key_exists($lookup, $pie_type)) {
+            $output = '<div>' . esc_html($pie_type[$lookup]) . '</div>';
         } else {
             // If 'lookup' is not provided or does not exist, return all pie types.
             $output .= '<div>Available pie types:</div><ul>';
-            foreach ($pies as $key => $pie) {
+            foreach ($pie_type as $key => $pie) {
                 $output .= '<li>' . esc_html($pie) . '</li>';
             }
             $output .= '</ul>';
